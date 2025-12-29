7214e4b55173407a6a1e3759596a16e9ebeafff3
0 <zadoronidanil@gmail.com>
2025-12-30 03:51:42 +0700

    Update Binds.lua

--- Binds.lua
+++ Binds.lua
@@ -221,7 +221,7 @@ function Binds.Init(nxs)
     Binds.CreateDisplayGUI()
     
     -- ========== SURVIVOR BINDS ==========
-    Tabs.Binds:AddSection("Survivor Binds")
+   Tabs.Binds:AddSection("Section", "snowflake")
     
     -- AutoParry
     local AutoParryKeybind = Tabs.Binds:AddKeybind("AutoParryKeybind", {
